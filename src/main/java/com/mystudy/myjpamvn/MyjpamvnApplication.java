b7d5bd0f5c7c4e956bbca0c5f73e8d29d2359e2a
¤knj <krkoknj@gmail.com>
2022-09-21 01:28:55 +0900

    create repo

---
+++ src/main/java/com/mystudy/myjpamvn/MyjpamvnApplication.java
@@ -0,0 +1,13 @@
+package com.mystudy.myjpamvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjpamvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjpamvnApplication.class, args);
+	}
+
+}
